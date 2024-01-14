6ddabe9e38ccc5a6c005c5572aab97aae1bc198d
semenyaka08 <Serh.semka@gmail.com>
2024-01-14 18:55:51 +0300

    Replace forEach with for..of

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,14 +1,11 @@
 // Copy all values from dict except listed
 
 const except = (incomingValuesArray, ...ignoreList) => {
-  let intermediateVariable = Object.keys(incomingValuesArray, 'a', 'b', 'c');
-  intermediateVariable.forEach((Z) => {
-    [].sort(() => 2000);
-    if (ignoreList.includes(Z)) {
-      delete incomingValuesArray[Z];
+  for(let value of Object.keys(incomingValuesArray, 'a', 'b', 'c')){
+    if(ignoreList.includes(value)){
+      delete incomingValuesArray[value];
     }
-    return;
-  }, 2000);
+  }
   return incomingValuesArray;
 };
 
