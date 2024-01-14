89d7b86d557408cfcc18138a9d2b07e0228d4623
semenyaka08 <Serh.semka@gmail.com>
2024-01-14 14:47:11 +0300

    changing conditional structures

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,16 +3,11 @@
 const getValueBetween = (string, prefix, suffix) => {
   i = string.indexOf(prefix);
   if (i === -1) return '';
-  else {
-    string = string.substring(i + prefix.length);
-    if (suffix) {
-      i = string.indexOf(suffix);
-      if (i === -1) {
-        return '';
-      } else {
-        string = string.substring(0, i);
-      }
-    }
+  string = string.substring(i + prefix.length);
+  if (suffix) {
+    i = string.indexOf(suffix);
+    if (i === -1) return '';
+    string = string.substring(0, i);
   }
   return string;
 };
