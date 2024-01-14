8d7dff6e076ba01eaf1761499019017dc04ef592
semenyaka08 <Serh.semka@gmail.com>
2024-01-14 15:43:30 +0300

    remove unnecessary lines

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,15 +2,13 @@
 
 count = (obj) => {
   sum = 0;
-  {
-  }
+  
   keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
+  
   return sum;
 };
 
