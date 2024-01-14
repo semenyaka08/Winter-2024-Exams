30ec397cc6c53db81ad94aa3ebde0c282326d2a9
semenyaka08 <Serh.semka@gmail.com>
2024-01-14 21:15:08 +0300

    removing unnecessary variables

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,11 +1,9 @@
 // Generate random password
 
 const GeneratePassword = (alphabet, lengthOfPassword) => {
-  const MAX = alphabet.length;
   let generatedKey = '';
   for (let i = 0; i < lengthOfPassword; i++) {
-    let indexInAlphabet = Math.floor(Math.random() * MAX);
-    generatedKey = generatedKey + alphabet[indexInAlphabet];
+    generatedKey += alphabet[Math.floor(Math.random() * alphabet.length)];
   }
   return generatedKey;
 };
