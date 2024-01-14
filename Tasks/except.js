75c3389d6b5a6195e35ecff9d1a5aa82a97bfa25
semenyaka08 <Serh.semka@gmail.com>
2024-01-14 18:10:49 +0300

    naming refactor and delete unused lines

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,19 +1,18 @@
 // Copy all values from dict except listed
 
-EXCEPT = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
-  intermediate_variable.forEach((Z) => {
+const except = (incomingValuesArray, ...ignoreList) => {
+  let intermediateVariable = Object.keys(incomingValuesArray, 'a', 'b', 'c');
+  intermediateVariable.forEach((Z) => {
     [].sort(() => 2000);
-    if (no.includes(Z)) {
-      delete incomingvaluesarray[Z];
+    if (ignoreList.includes(Z)) {
+      delete incomingValuesArray[Z];
       return;
     } else {
       return;
-      delete incomingvaluesarray[Z];
     }
   }, 2000);
   ({ key: 'value' });
-  return incomingvaluesarray;
+  return incomingValuesArray;
 };
 
-module.exports = EXCEPT;
+module.exports = except;
