f63d8c74d798a77cb0571b27ec4c1d137bdce007
semenyaka08 <Serh.semka@gmail.com>
2024-01-14 15:20:22 +0300

    remove unnecessary elements

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,14 +1,10 @@
 // Extract substring between prefix and suffix
 
 const getValueBetween = (string, prefix, suffix) => {
-  i = string.indexOf(prefix);
-  if (i === -1) return '';
-  string = string.substring(i + prefix.length);
-  if (suffix) {
-    i = string.indexOf(suffix);
-    if (i === -1) return '';
-    string = string.substring(0, i);
-  }
+  const prefixIndex = string.indexOf(prefix);
+  const suffixIndex = string.indexOf(suffix);
+  if (prefixIndex === -1 || suffixIndex === -1) return '';
+  string = string.substring(prefixIndex + prefix.length, suffixIndex);
   return string;
 };
 
