8f4d388912f6e9d7404a953c898ef9414c33f295
semenyaka08 <Serh.semka@gmail.com>
2024-01-14 14:23:00 +0300

    naming refactor

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,21 +1,21 @@
 // Extract substring between prefix and suffix
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p);
+const getValueBetween = (string, prefix, suffix) => {
+  i = string.indexOf(prefix);
   if (i === -1) return '';
   else {
-    k = i + p.length;
-    str = str.substring(k);
-    if (s) {
-      i = str.indexOf(s);
+    k = i + prefix.length;
+    string = string.substring(k);
+    if (suffix) {
+      i = string.indexOf(suffix);
       if (i === -1) {
         return '';
       } else {
-        str = str.substring(0, i);
+        string = string.substring(0, i);
       }
     }
   }
-  return str;
+  return string;
 };
 
-module.exports = getvaluebetween;
+module.exports = getValueBetween;
